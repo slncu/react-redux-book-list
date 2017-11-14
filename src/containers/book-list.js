f8c6d95fddff5c29d86ffaee84d6c69a2f4d13d3
gochiUSa <sinc@gpchiUSa.local>
2017-11-14 22:56:04 +0900

    initial commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul className="">{book.name}
+      </ul>
+    );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  [
+    { title: 'JavaScript the good parts'},
+    { title: 'Harry Potter'},
+    { title: 'The Dark Tower'},
+    { title: 'Eloquent Ruby'}
+  ]
+}
